70d06b057305d4a8d2b9578a84b651e71301283a
Siddhant Agarwal <siddhantagarwal99@gmail.com>
2018-10-21 06:00:30 +0530

    Changes

--- messtally/static/javascript/mess.js
+++ messtally/static/javascript/mess.js
@@ -2,3 +2,11 @@ $('.message a').click(function(){
    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
 "use strict";
+function addrow(){
+  var table=document.getelemntbyid("ind");
+  var row = table.insertRow(0);
+  var cell1 = row.insertcell(0);
+  var cell2 = row.insertcell(1);
+  cell1.innerHTML = "New";
+  cell2.innerHTML = "new";
+}
